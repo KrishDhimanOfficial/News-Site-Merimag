4388f84387b8b94ade68cc8be54da8ac05222a90
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-04-09 00:40:35 +0530

    completed half of the css

--- javascript/index.js
+++ javascript/index.js
@@ -9,11 +9,38 @@ search_btn.addEventListener("click", ()=>{
 const colors = ["red","green","purple","blue","orange","yellow","lightblue"];
 const set_prop = document.querySelectorAll("#news_masonary .news");
 
-let i = 0;
+var i = 0;
 set_prop.forEach(set_prop => {
     set_prop.style.setProperty("background-image", `linear-gradient(138deg , transparent, ${colors[i]})`);
     i < colors.length ? ++i :false;
 });
+
+// scoial media
+var links_colors = ["#647aad","#7abdf2","#d95250","#b0968b","#d95050","#5cc5ee"];
+var scoial_media = document.querySelectorAll(".scoial_media i");
+
+var i = 0;
+scoial_media.forEach(scoial_media =>{
+    scoial_media.style.backgroundColor = `${links_colors[i]}` ;
+    i < links_colors.length ? ++i :false;
+});
+
+var fixed = document.querySelector("#s_media");
+window.addEventListener("scroll",()=>{
+    var top = window.scrollY;
+    var left = fixed.offsetLeft;
+    console.clear();
+    console.log(top);
+    
+    if( top >= 904){
+        fixed.classList.add("fixed");
+        fixed.style.top =`${0}px`;
+        fixed.style.left =`${left}px`;
+    }else{
+        fixed.classList.remove("fixed");
+    }
+});
+
 
 
 
