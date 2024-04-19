992137f7edd361b3662e16818d3ad75d976b95e2
KRISH DHIMAN <krrishdhiman841@gmail.com>
2024-04-20 00:42:29 +0530

    formatted html and done news masnory

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,7 @@ search_btn.addEventListener("click", ()=>{
 });
 
 // news masonry
-const colors = ["red","green","purple","blue","orange","lightblue"];
+const colors = ["red","green","purple","blue","orange","#7fe5f0"];
 const set_prop = document.querySelectorAll("#news_masonary .news");
 
 var i = 0;
@@ -26,21 +26,20 @@ scoial_media.forEach(scoial_media =>{
 });
 
 var fixed = document.querySelector("#s_media");
-window.addEventListener("scroll",()=>{
+
+window.addEventListener("scroll", () => {
     var top = window.scrollY;
     var left = fixed.offsetLeft;
-    console.clear();
-    console.log();
-    
-    if( top >= 904 && top <= 1750){
+
+    if (top >= 904 && top <= 1750) {
         fixed.classList.add("fixed");
-        fixed.style.top =`${0}px`;
-        fixed.style.left =`${left}px`;
-    }
-    else{
+        fixed.style.top = `${0}px`;
+        fixed.style.left = `${left}px`;
+    } else {
         fixed.classList.remove("fixed");
     }
 });
+
 
 
 
